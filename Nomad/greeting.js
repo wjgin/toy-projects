ac56f695404c6206e6805c442b5998f353990573
wjgin <woojin0913@naver.com>
2021-03-23 23:46:23 +0900

    local storage

---
+++ Nomad/greeting.js
@@ -0,0 +1,40 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greeting");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function paintingName(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintingName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+    
+}
+
+function loadUser() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        askForName();
+    } else {
+        paintingName(currentUser);
+    }
+}
+
+function init() {
+    loadUser();
+}
+
+init();
+
